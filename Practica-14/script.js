8ffe855174fbfd2b412f5e50252f79d6e8850f95
Kevin Montano <2022kevinmontano@gmail.com>
2023-12-05 14:50:38 -0600

    archivos de parctica 14

--- Practica-14/script.js
+++ Practica-14/script.js
@@ -1,3 +1,4 @@
+// fucion para sumar
 function sumar(){
     var num1 = parseFloat(document.getElementById("numero1").value)
 
@@ -8,6 +9,7 @@ function sumar(){
     mostrarResultado(resultado)
 }
 
+// funcion para restar
 function restar(){
     var num1 = parseFloat(document.getElementById("numero1").value)
 
@@ -18,6 +20,7 @@ function restar(){
     mostrarResultado(resultado)
 }
 
+// funcion para multiplicar
 function multiplicar(){
     var num1 = parseFloat(document.getElementById("numero1").value)
 
@@ -28,23 +31,29 @@ function multiplicar(){
     mostrarResultado(resultado)
 }
 
+
+// funcion para dividir
 function dividir(){
     var num1 = parseFloat(document.getElementById("numero1").value)
 
     var num2 = parseFloat(document.getElementById("numero2").value)
 
-    if (num1 === 0 ) {
+    if (num2 === 0 ) {
         var resultado = "No se puede dividir entre cero"
         mostrarResultado(resultado)
     } else {
         var resultado = num1 / num2
         mostrarResultado(resultado)
-    }
-
-    
+    }   
 }
 
+
+// funcion para mostrar resultado
 function mostrarResultado(resultado){
-    document.getElementById("resultado").innerHTML = "Resultado " + resultado.toFixed(2)
+    if (resultado.typeof === "number"){
+        document.getElementById("resultado").innerHTML = "Resultado: " + resultado.toFixed(2)
+    } else {
+        document.getElementById("resultado").innerHTML = "Resultado: " + resultado
+    }
 
 }
